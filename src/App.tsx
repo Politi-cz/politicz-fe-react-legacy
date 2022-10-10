5adbf9b9904bf50565cf933b1fb649d57d279bda
Martin Koller <kollermartin00@gmail.com>
2022-10-10 21:10:00 +0200

    Add Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Button from '@mui/material/Button';
 import React from 'react';
 import './App.scss';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <Button variant="contained">Hello World</Button>
   );
 }
 
