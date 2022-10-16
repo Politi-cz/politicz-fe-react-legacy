541c4d65b79540eb6d64b39cfc3015b3aed1a05d
Martin Koller <kollermartin00@gmail.com>
2022-10-16 21:14:26 +0200

    toggle language

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import Button from '@mui/material/Button';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { useTranslation } from 'react-i18next';
 
 function App() {
   const { t, i18n } = useTranslation();
+  const [lang, setLang] = useState('cz');
 
-  function changeToEn(): void {
-    i18n.changeLanguage('en');
+  useEffect(() => {
+    i18n.changeLanguage(lang);
+  }, [lang]);
+
+  function changeLanguage(): void {
+    if (lang === 'cz') {
+      setLang('en');
+    }
+
+    if (lang === 'en') {
+      setLang('cz');
+    }
   }
 
   return (
     <>
       <h1 data-testid="h-trans">{t('welcome')}</h1>
-      <Button data-testid="btn-trans" onClick={changeToEn} variant="contained">
+      <Button data-testid="btn-trans" onClick={changeLanguage} variant="contained">
         {t('hello_world')}
       </Button>
     </>
