a90a2bea7c3813b9268642bd868b3e94d65fb4de
Martin Koller <kollermartin00@gmail.com>
2022-10-17 21:17:33 +0200

    Add settings menu to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import Button from '@mui/material/Button';
 import {} from '@mui/material/colors';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.scss';
-import { useTranslation } from 'react-i18next';
 import { Navbar } from './components/Navbar';
 
 function App() {
   const theme = createTheme({
     palette: {
       primary: {
-        main: '#4B7BF5',
+        main: '#4B7BF5'
       },
       secondary: {
-        main: '#FFF',
-      },
-    },
+        main: '#FFF'
+      }
+    }
   });
 
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,57 @@
-import { AppBar, Box, IconButton, Tab, Tabs, Toolbar } from '@mui/material';
+import { AppBar, Box, IconButton, Menu, MenuItem, Tab, Tabs, Toolbar } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useState } from 'react';
 import HomeIcon from '@mui/icons-material/Home';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { useTranslation } from 'react-i18next';
+import React from 'react';
 
 export function Navbar() {
   const [tabs, setUseTabs] = useState(0);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const { t } = useTranslation();
+
+  const open = Boolean(anchorEl);
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
     setUseTabs(newValue);
+  };
+
+  const handeSettingsMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleSettingsMenuClose = () => {
+    setAnchorEl(null);
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="secondary"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
+          <IconButton size="large" edge="start" color="secondary" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
           <Tabs
-            textColor='secondary'
-            indicatorColor='secondary'
+            textColor="secondary"
+            indicatorColor="secondary"
             value={tabs}
             onChange={handleTabChange}
-          >
-            <Tab icon={<HomeIcon />} iconPosition="start" label="start" />
-            <Tab icon={<HomeIcon />} iconPosition="start" label="start" />
-
+            sx={{ flexGrow: 1 }}>
+            <Tab icon={<HomeIcon />} iconPosition="start" label={t('nav-item_main')} />
           </Tabs>
+          <IconButton size="large" color="inherit" onClick={handeSettingsMenuClick}>
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="settings-menu"
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleSettingsMenuClose}>
+            <MenuItem>{t('menu_settings_profile')}</MenuItem>
+            <MenuItem>{t('menu_settings_logout')}</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
